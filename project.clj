295eddaeeb28d2ed15140f167e651be56108c38b
Alexander Kiel <alexanderkiel@gmx.net>
2016-02-13 19:53:50 +0100

    Bump Next Dev Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/pull "0.1"
+(defproject org.clojars.akiel/pull "0.2-SNAPSHOT"
   :description "Pull like in Datomic but over maps of maps."
   :url "https://github.com/alexanderkiel/pull"
   :license {:name "Eclipse Public License"
